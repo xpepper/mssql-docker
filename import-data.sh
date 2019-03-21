68fee5552dfb7c38c9aa24619a5d1a3931150c1d
Pietro Di Bello <pierodibello@gmail.com>
2019-03-22 00:45:05 +0100

    avoid to create and populate the authentications table

--- import-data.sh
+++ import-data.sh
@@ -2,4 +2,4 @@
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 3DS2Issuer -d master -i setup.sql
 
 #import the data from the csv file
-/opt/mssql-tools/bin/bcp IssuerServices.dbo.authentications in "/usr/src/app/authentications.csv" -c -t',' -S localhost -U sa -P 3DS2Issuer
+#/opt/mssql-tools/bin/bcp IssuerServices.dbo.authentications in "/usr/src/app/authentications.csv" -c -t',' -S localhost -U sa -P 3DS2Issuer
--- setup.sql
+++ setup.sql
@@ -1,9 +1,8 @@
 CREATE DATABASE IssuerServices;
 GO
 
-USE IssuerServices;
-GO
+-- USE IssuerServices;
+-- GO
 
-CREATE TABLE authentications (id INTEGER IDENTITY, authenticationId VARCHAR(50) UNIQUE, userAccountId VARCHAR(50), status VARCHAR(50), challengeType VARCHAR(50), acsTransactionId VARCHAR(50));
-GO
-
+-- CREATE TABLE authentications (id INTEGER IDENTITY, authenticationId VARCHAR(50) UNIQUE, userAccountId VARCHAR(50), status VARCHAR(50), challengeType VARCHAR(50), acsTransactionId VARCHAR(50));
+-- GO
